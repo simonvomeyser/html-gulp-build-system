a43420d1eae242b5ea359a4142ace8dc33cc6c7d
Simon vom Eyser <simon.vom.eyser@gmail.com>
2016-05-15 21:35:50 +0200

    initial

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var pug = require('gulp-pug');
+
+var autoprefixer = require('gulp-autoprefixer');
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var historyApiFallback = require('connect-history-api-fallback')
+
+/*
+  Styles Task
+*/
+
+gulp.task('styles',function() {
+
+  // move over fonts
+  // gulp.src('css/fonts/**.*')
+  //   .pipe(gulp.dest('dist/css/fonts'))
+
+  // Compiles CSS
+  gulp.src(['./css/style.styl'])
+    .pipe(stylus())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(reload({stream:true}));
+    
+    // .pipe(autoprefixer())
+    // .pipe(reload({stream:true}))
+});
+
+gulp.task('html', function () {
+  gulp.src(['./html/**.*'])
+    .pipe(pug({
+          pretty: true
+          }))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(reload({stream:true}));
+})
+
+
+/*
+  Browser Sync
+*/
+gulp.task('browser-sync', function() {
+    browserSync({
+        // we need to disable clicks and forms for when we test multiple rooms
+        server : {
+          baseDir: './dist/'
+        },
+        ghostMode: false
+    });
+});
+
+
+// run 'scripts' task first, then watch for future changes
+gulp.task('default', ['styles', 'html', 'browser-sync'], function() {
+  gulp.watch('css/**/*', ['styles']); // gulp watch for stylus changes
+  gulp.watch('html/**/*', ['html']);
+ // gulp watch for html changes
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+# html-gulp-build-system
+
+Small and simple build system I created for frontend/design work e.g. when you have to effectively transform a *designers vision* to HMTL or prefer to design in the browser.
+
+Based on:
+- pug (https://github.com/pugjs/pug) for html templating
+- stylus (http://stylus-lang.com/) 
+- browser sync (https://www.browsersync.io/) for live reloading
+
+
+
+
+
